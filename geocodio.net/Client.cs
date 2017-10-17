0d297d75c6f07f8fb3183046b58587e086992cde
emily kodl <emily.coate@amsec.hii-nns.com>
2017-10-16 22:22:02 -0400

    init commit try 2 :)

---
+++ geocodio.net/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace geocodio.net
+{
+    class Client
+    {
+        public string APIKey { get; set; }
+
+        public bool SecureConnection { get; set; }
+
+
+    }
+}
