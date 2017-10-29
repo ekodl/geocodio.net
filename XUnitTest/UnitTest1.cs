7ca7e7d9fb2ddb2dfca4b60be36dee967b25dcda
emily kodl <emily.coate@amsec.hii-nns.com>
2017-10-29 17:50:18 -0400

    plannned and scaffolded methods for client class, xunit tests need more research. Added a GeocodioException to deal with stuff coming back from Geocod.io.

--- XUnitTest/UnitTest1.cs
+++ XUnitTest/UnitTest1.cs
@@ -1,14 +1,17 @@
+using geocodio.net;
 using System;
 using Xunit;
+
 
 namespace XUnitTest
 {
     public class UnitTest1
     {
+        private Client _client;
+
         [Fact]
         public void Test1()
         {
-
         }
     }
 }
