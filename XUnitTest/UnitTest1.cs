42919062f432db6ead1bfc9d4de8a87ea33bf39e
emily kodl <emily.coate@amsec.hii-nns.com>
2017-10-31 20:54:48 -0400

    initial implementation and tests for geocode and reverse geocode.  also tests for the post method that i wrote previously - i think i merged branches incorrectly?

--- XUnitTest/UnitTest1.cs
+++ XUnitTest/UnitTest1.cs
@@ -1,5 +1,6 @@
 using geocodio.net;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 
@@ -8,6 +9,7 @@ namespace XUnitTest
     public class UnitTest1
     {
         private Client _client;
+        private const string _apiKey = "d97f0308d7991f7f7a0acbc9dc3899548153315";
 
         [Fact]
         public async void Post_InvalidArguments_ThrowArgumentExceptionAsync()
@@ -39,7 +41,7 @@ namespace XUnitTest
         [Fact]
         public async void Post_BadAddressData_Catches422()
         {
-            _client = new Client("YOUR API KEY HERE");
+            _client = new Client(_apiKey);
 
             await Assert.ThrowsAsync<GeocodioException>(() => _client.Post("reverse", "123 Under Da Sea"));
         }
@@ -48,11 +50,78 @@ namespace XUnitTest
         [Fact]
         public async void Post_ValidArg_DoesNotThrowExceptionAsync()
         {
-            _client = new Client("YOUR API KEY HERE", false);
+            _client = new Client(_apiKey);
 
             var returnStr = await _client.Post("geocode", @"[""5200 W Mercury Blvd, Newport News, VA""]");
 
             Assert.NotNull(returnStr);
+        }
+
+        [Fact]
+        public async void Geocode_InvalidArg_ThrowsArgumentExceptionAsync()
+        {
+            _client = new Client(_apiKey);
+            List<string> list = null;
+            await Assert.ThrowsAsync<ArgumentNullException>(() => _client.Geocode(list));
+        }
+
+        //TODO: turn the following into theories
+        [Fact]
+        public async void Geocode_OneAddress_Success()
+        {
+            _client = new Client(_apiKey);
+            List<string> list = new List<string>() {"5200 W Mercury Blvd, Newport News, VA"};
+
+            var coord = await _client.Geocode(list);
+
+            Assert.NotNull(coord);
+            Assert.NotEmpty(coord);
+        }
+
+        [Fact]
+        public async void Geocode_MultipleAddresses_Success()
+        {
+            _client = new Client(_apiKey);
+            List<string> list = new List<string>() { "5200 W Mercury Blvd, Newport News, VA", "4101 Washington Ave, Newport News, VA 23607" };
+
+            var coord = await _client.Geocode(list);
+
+            Assert.NotNull(coord);
+            Assert.NotEmpty(coord);
+        }
+
+
+        [Fact]
+        public async void ReverseGeocode_InvalidArg_ThrowsArgumentExceptionAsync()
+        {
+            _client = new Client(_apiKey);
+            List<Tuple<double, double>> list = null;
+            await Assert.ThrowsAsync<ArgumentNullException>(() => _client.ReverseGeocode(list));
+        }
+
+        //TODO: turn the following into theories
+        [Fact]
+        public async void ReverseGeocode_OneAddress_Success()
+        {
+            _client = new Client(_apiKey);
+            List<Tuple<double, double>> list = new List<Tuple<double, double>>() { new Tuple<double, double>(36.9882362,-76.4371783)};
+
+            var addr = await _client.ReverseGeocode(list);
+
+            Assert.NotNull(addr);
+            Assert.NotEmpty(addr);
+        }
+
+        [Fact]
+        public async void ReverseGeocode_MultipleAddresses_Success()
+        {
+            _client = new Client(_apiKey);
+            List<Tuple<double, double>> list = new List<Tuple<double, double>>() { new Tuple<double, double>(36.9882362, -76.4371783), new Tuple<double, double>(37.0236644, -76.4403612) };
+
+            var addr = await _client.ReverseGeocode(list);
+
+            Assert.NotNull(addr);
+            Assert.NotEmpty(addr);
         }
     }
 }
